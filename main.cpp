d3958841a72dc8814db3bb605674e01490fd9c14
juliengirod <juliengirod12@gmail.com>
2022-10-09 22:52:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@
  * GRADING to 0.
  */
 
-#define GRADING 1
+#define GRADING 0
 
 #include <iostream>
 #include "grading/grading.hpp"
@@ -35,6 +35,7 @@ int main(int argc, char* argv[])
   // This code will be executed only if you set GRADING to a value different from 1
 
   {
+  std::cout << "First git commit" << std::endl;
   }
   // END OF THE CUSTOM CODE SECTION
 #else
@@ -49,5 +50,5 @@ int main(int argc, char* argv[])
     return tdgrading::grading(std::cerr, test_number);
     // END OF THE AUTOMATIC GRADER
   }
-#endif
+#endif 
 }
