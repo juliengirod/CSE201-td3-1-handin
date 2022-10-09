e361e7b5487dbbfd4a29b185cfcd4b80733e1f29
juliengirod <juliengirod12@gmail.com>
2022-10-09 23:34:42 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char* argv[])
   // This code will be executed only if you set GRADING to a value different from 1
 
   {
-  std::cout << "A better, New initial message" << std::endl;
+  std::cout << "A better better, New initial message" << std::endl;
   }
   // END OF THE CUSTOM CODE SECTION
 #else
@@ -52,4 +52,4 @@ int main(int argc, char* argv[])
   }
 #endif 
 }
-}
+
